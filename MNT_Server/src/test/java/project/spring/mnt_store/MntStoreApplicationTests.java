7db02212dabfb13a642057a4501d1895a8a2fd59
Bernardo Mira Rodrigues <A44784@alunos.isel.pt>
2022-11-15 18:30:48 +0000

    Backend Spring Project Creation

---
+++ MNT_Server/src/test/java/project/spring/mnt_store/MntStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package project.spring.mnt_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MntStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
